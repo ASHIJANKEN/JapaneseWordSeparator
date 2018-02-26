de4e90cdc6463866acf6f628b38be4952d51e975
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-02-26 13:51:04 +0900

    modify segmenter from js to python

---
+++ japanesewordseparator.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+
+
+class DragSelectJapaneseCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    expand_by_class(point, classes, <separators>) 
+    print(self.view.substr(self.view.sel()[0]))
+    # self.view.insert(edit, 0, self.view.sel())
