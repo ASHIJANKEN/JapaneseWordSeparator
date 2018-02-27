cb534745a7a74c7d475602774d2ffdca2771c54c
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-02-27 12:22:48 +0900

    fix problem that double-click doesn't work

--- japanesewordseparator.py
+++ japanesewordseparator.py
@@ -2,8 +2,34 @@ import sublime
 import sublime_plugin
 
 
-class DragSelectJapaneseCommand(sublime_plugin.TextCommand):
+#TextCommandクラスよりもこっちが先に呼び出される
+class JapaneseDragSelectCommand(sublime_plugin.EventListener):
+  def on_text_command(self, view, command_name, args):
+    if command_name == "drag_select_jp":
+      print("drag_select_called")
+      # view.run_command("drag_select", {"by": "words"})
+
+      # self.view.sel().clear()
+      # print(view.sel()[0])
+      # point = view.sel()[0].b
+      # view.sel().clear()
+      # region = sublime.Region(point - 1, point + 1)
+      # view.sel().add(region)
+      # print(view.sel()[0])
+      # self.view.sel().add([region])
+    elif command_name == "drag_select_additive_jp":
+      print("drag_select_additive_called")
+    elif command_name == "drag_select_subtractive_jp":
+      print("drag_select_subtractive_called")
+      # expand_by_class(point, classes, <separators>) 
+      print(view.substr(self.view.sel()[0]))
+      # self.view.insert(edit, 0, self.view.sel())
+
+class DragSelectJp(sublime_plugin.TextCommand):
   def run(self, edit):
-    expand_by_class(point, classes, <separators>) 
-    print(self.view.substr(self.view.sel()[0]))
-    # self.view.insert(edit, 0, self.view.sel())
+    print(self.view.sel()[0])
+    point = self.view.sel()[0].b
+    self.view.sel().clear()
+    region = sublime.Region(point - 1, point + 3)
+    self.view.sel().add(region)
+    print(self.view.sel()[0])
