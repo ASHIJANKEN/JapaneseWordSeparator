3855561553d2655076fc1a1ef5b517501413d5d1
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-03-09 22:15:43 +0900

    fix bug in segmenter

--- japanesewordseparator.py
+++ japanesewordseparator.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import sublime
 import sublime_plugin
+from tinysegmenter_python import segment
 
+FIND_SPAN = 10
 
 #TextCommandクラスよりもこっちが先に呼び出される
 class JapaneseDragSelectCommand(sublime_plugin.EventListener):
@@ -30,6 +36,19 @@ class DragSelectJp(sublime_plugin.TextCommand):
     print(self.view.sel()[0])
     point = self.view.sel()[0].b
     self.view.sel().clear()
-    region = sublime.Region(point - 1, point + 3)
+
+    # Find segment from canditate_str
+    canditate = sublime.Region(point - FIND_SPAN, point + FIND_SPAN)
+    canditate_str = self.view.substr(canditate)
+    segs = segment(canditate_str)
+    sum_chars = 0
+    for seg in segs:
+      sum_chars += len(seg)
+      if sum_chars >= FIND_SPAN:
+        point_seg = sublime.Region((sum_chars - len(seg)) + (point - FIND_SPAN), point - FIND_SPAN + sum_chars)
+        break
+
+    region = point_seg
+    # region = sublime.Region(point - 1, point + 3)
     self.view.sel().add(region)
     print(self.view.sel()[0])
