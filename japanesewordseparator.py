26f930f374921220b025c5085483490c1810be40
ASHIJA-WIN-FRONTIER <tortoise.mountain.3408@gmail.com>
2018-03-10 13:19:19 +0900

    update segment system

--- japanesewordseparator.py
+++ japanesewordseparator.py
@@ -5,15 +5,14 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import sublime
 import sublime_plugin
 from tinysegmenter_python import segment
-
-FIND_SPAN = 10
+import re
 
 #TextCommandクラスよりもこっちが先に呼び出される
 class JapaneseDragSelectCommand(sublime_plugin.EventListener):
   def on_text_command(self, view, command_name, args):
     if command_name == "drag_select_jp":
       print("drag_select_called")
-      # view.run_command("drag_select", {"by": "words"})
+      view.run_command("drag_select", {"by": "words"})
 
       # self.view.sel().clear()
       # print(view.sel()[0])
@@ -33,22 +32,31 @@ class JapaneseDragSelectCommand(sublime_plugin.EventListener):
 
 class DragSelectJp(sublime_plugin.TextCommand):
   def run(self, edit):
-    print(self.view.sel()[0])
     point = self.view.sel()[0].b
     self.view.sel().clear()
 
-    # Find segment from canditate_str
-    canditate = sublime.Region(point - FIND_SPAN, point + FIND_SPAN)
-    canditate_str = self.view.substr(canditate)
+    # STの機能で単語選択
+    canditate_region = self.view.word(point)
+    canditate_str = self.view.substr(canditate_region)
+
+    # 選択範囲に日本語がなければregionの確定
+    if not re.search(u'[一-龠々〆ヵヶぁ-んァ-ヴｱ-ﾝﾞ]', canditate_str):
+      self.view.sel().add(canditate_region)
+      return
+
+    # 日本語があったらsegmenterをsegmentを見つける
     segs = segment(canditate_str)
+    # print(segs)
+    if len(segs) == 1:
+      self.view.sel().add(canditate_region)
+      return
     sum_chars = 0
     for seg in segs:
+      print(seg)
       sum_chars += len(seg)
-      if sum_chars >= FIND_SPAN:
-        point_seg = sublime.Region((sum_chars - len(seg)) + (point - FIND_SPAN), point - FIND_SPAN + sum_chars)
+      if sum_chars >= point - canditate_region.a:
+        point_seg = sublime.Region((sum_chars - len(seg)) + canditate_region.a, canditate_region.a + sum_chars)
+        # print("({}) ({})".format((sum_chars - len(seg)) + canditate_region.a, point - canditate_region.a + sum_chars))
         break
 
-    region = point_seg
-    # region = sublime.Region(point - 1, point + 3)
-    self.view.sel().add(region)
-    print(self.view.sel()[0])
+    self.view.sel().add(point_seg)
